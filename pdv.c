424731554442d53b7c32d47c7968af6f8fa22d44
Dexeat <Dexeat@users.noreply.github.com>
2020-01-13 17:11:04 +0100

    Create pdv.c

---
+++ pdv.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+int pvmonster;
+int attack;
+pvmonster=100;
+attack=20;
+
+printf("Vous infligez 20 points de degats \n");
+pvmonster=pvmonster-attack;
+
+printf("Heinrich a %d",pvmonster);
+printf(" points de vie");
+}
